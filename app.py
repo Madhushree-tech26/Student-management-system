30486c446126fb7dfa397458c62f79350c63930d
Madhushree <madhushreevibesh@gmail.com>
2026-09-17 20:34:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -522,4 +522,4 @@ def students_page():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
